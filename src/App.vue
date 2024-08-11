37cd7a1d278b2359836b887ec83b29d4f513c108
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-08-11 22:31:17 +0300

    deleted Router link in app vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-   <header>
-      <nav>
-         <RouterLink to="/">Home</RouterLink>
-      </nav>
-   </header>
    <RouterView />
 </template>
 
