6209d119b5e5f28acd78125956eb654059b03503
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-08-12 17:06:08 +0300

    added feyth in news
    Co-Authored-By: Volodya Mikhnov <67098357+Vova445@users.noreply.github.com>

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -3,7 +3,23 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import MainMasterPage from '../masterPages/MainMasterPage.vue'
+const apiUrl = 'https://dev.to/api/articles?per_page=10&tag=programming&top=1'
+
+onMounted(() => {
+   async function fetchProgrammingNews() {
+      try {
+         const response = await fetch(apiUrl)
+         const newsData = await response.json()
+         const topNews = newsData.slice(0, 10)
+         console.log(topNews)
+      } catch (error) {
+         console.error('Помилка при отриманні новин:', error)
+      }
+   }
+   fetchProgrammingNews()
+})
 </script>
 
 <style lang="scss" scoped></style>
