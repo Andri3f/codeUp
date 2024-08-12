e8582d544cbe461acf0f82bb71f9925576f28bc1
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-08-12 16:20:44 +0300

    u6trgfd

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './assets/styles/index.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { VueEmotion } from '@egoist/vue-emotion'
 
 //import i18n
 import i18n from './plugins/i18n'
@@ -14,5 +13,4 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
-app.use(VueEmotion)
 app.mount('#app')
