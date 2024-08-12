5e7798bf00e2494c4b4e093d2b548b22f1b82841
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-08-12 23:55:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-//export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//      alias: {
-//         '@': fileURLToPath(new URL('./src', import.meta.url)),
-//      },
-//   },
-//   build: {
-//      outDir: 'dist',
-//   },
-//})
 export default defineConfig({
    plugins: [vue()],
    resolve: {
@@ -25,5 +14,16 @@ export default defineConfig({
    build: {
       outDir: 'dist',
    },
-   base: '/codeup/',
 })
+//export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//      alias: {
+//         '@': fileURLToPath(new URL('./src', import.meta.url)),
+//      },
+//   },
+//   build: {
+//      outDir: 'dist',
+//   },
+//   base: '/codeup/',
+//})
