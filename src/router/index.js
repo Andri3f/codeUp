67836d7f416603a7d40e9c6ef356ce6e951520f3
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-08-13 15:49:19 +0300

    d

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,68 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
-   { path: '/', name: 'home', component: HomeView, meta: { requireAuth: false } },
-   { path: '/news', name: 'news', component: () => import('../views/NewsPage.vue'), meta: { requireAuth: false } },
-   {
-      path: '/contacts',
-      name: 'contacts',
-      component: () => import('../views/ContactsPage.vue'),
-      meta: { requireAuth: false },
-   },
-   {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/RegisterPage.vue'),
-      meta: { requireAuth: false },
-   },
-   { path: '/login', name: 'login', component: () => import('../views/LoginPage.vue'), meta: { requireAuth: false } },
-   { path: '/user', name: 'user', component: () => import('../views/UserPage.vue'), meta: { requireAuth: false } },
-   {
-      path: '/:pathMatch(.*)*',
-      name: 'notFound',
-      component: () => import('../components/error/ErrorComp.vue'),
-      meta: { requireAuth: false },
-   },
-]
-
 const router = createRouter({
    history: createWebHistory(import.meta.env.BASE_URL),
-   routes,
+   routes: [
+      {
+         path: '/',
+         name: 'home',
+         component: HomeView,
+         meta: {
+            requireAuth: false,
+         },
+      },
+      {
+         path: '/news',
+         name: 'news',
+         component: () => import('../views/NewsPage.vue'),
+         meta: {
+            requireAuth: false,
+         },
+      },
+      {
+         path: '/contacts',
+         name: 'contacts',
+         component: () => import('../views/ContactsPage.vue'),
+         meta: {
+            requireAuth: false,
+         },
+      },
+      {
+         path: '/register',
+         name: 'register',
+         component: () => import('../views/RegisterPage.vue'),
+         meta: {
+            requireAuth: false,
+         },
+      },
+      {
+         path: '/login',
+         name: 'login',
+         component: () => import('../views/LoginPage.vue'),
+         meta: {
+            requireAuth: false,
+         },
+      },
+      {
+         path: '/user',
+         name: 'user',
+         component: () => import('../views/UserPage.vue'),
+         meta: {
+            requireAuth: false,
+         },
+      },
+      {
+         path: '/:pathMatch(.*)*',
+         name: 'notFound',
+         component: () => import('../components/error/ErrorComp.vue'),
+         meta: {
+            requireAuth: false,
+         },
+      },
+   ],
 })
+
+//router.beforeEach(async (to) => {})
 
 export default router
