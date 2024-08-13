4e0c91c4779e7e6451e3e29d50cae5484f685829
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-08-13 15:36:31 +0300

    e

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,4 @@
-<script setup>
-import { onMounted, nextTick } from 'vue'
-
-onMounted(() => {
-   nextTick(() => {
-      requestAnimationFrame(() => {
-         document.documentElement.classList.add('loaded')
-      })
-   })
-
-   const updateResponsiveClass = () => {
-      if (window.innerWidth < 768) {
-         document.documentElement.classList.add('mobile')
-         document.documentElement.classList.remove('pc')
-      } else {
-         document.documentElement.classList.add('pc')
-         document.documentElement.classList.remove('mobile')
-      }
-   }
-
-   updateResponsiveClass()
-
-   window.addEventListener('resize', updateResponsiveClass)
-})
-</script>
+<script setup></script>
 <template>
    <RouterView />
 </template>
--- src/masterPages/MainMasterPage.vue
+++ src/masterPages/MainMasterPage.vue
@@ -9,6 +9,31 @@
 
 <script setup>
 import HeaderComp from '../components/header/HeaderComp.vue'
+import { onMounted, nextTick, onUnmounted } from 'vue'
+
+onMounted(() => {
+   nextTick(() => {
+      setTimeout(() => {
+         document.documentElement.classList.add('loaded')
+      }, 100)
+   })
+   const updateResponsiveClass = () => {
+      if (window.innerWidth < 768) {
+         document.documentElement.classList.add('mobile')
+         document.documentElement.classList.remove('pc')
+      } else {
+         document.documentElement.classList.add('pc')
+         document.documentElement.classList.remove('mobile')
+      }
+   }
+
+   updateResponsiveClass()
+
+   window.addEventListener('resize', updateResponsiveClass)
+})
+onUnmounted(() => {
+   document.documentElement.classList.remove('loaded')
+})
 </script>
 
 <style lang="scss" scoped></style>
